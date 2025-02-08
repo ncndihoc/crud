86956e78561ce4fd34b6367b69a04eccbde1c3e1
ncndev <ncnam20@vp.fitus.edu.vn>
2025-02-08 09:48:12 +0700

    Add Prisma ORM and restructure posts module

---
+++ src/routes/posts/posts.service.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../shared/services/prisma.service';
+@Injectable()
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getAllPosts() {
+    return this.prisma.post.findMany();
+  }
+
+  createPost(body: any) {
+    return this.prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: Number(body.authorId),
+      },
+    });
+  }
+
+  getPostById(id: string) {
+    return this.prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+  }
+
+  updatePost(id: string, body: any) {
+    return this.prisma.post.update({
+      where: { id: Number(id) },
+      data: body,
+    });
+  }
+
+  deletePost(id: string) {
+    return this.prisma.post.delete({
+      where: { id: Number(id) },
+    });
+  }
+}
